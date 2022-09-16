05000032510801c28156de348d557de4b83572ce
Patrick Keenan <pat@Patricks-MacBook-Pro.local>
2022-09-16 14:30:40 -0400

    fix(app): fixed the css file name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from '@types';
-import '../styles/globals.css';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
